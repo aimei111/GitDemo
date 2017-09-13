a7ad8431dff8821e1262026ff3beedbf7c9f14e4
aimei111 <574677347@qq.com>
2017-09-13 17:04:24 +0800

    测试上传

---
+++ LGGitDemo/LGGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LGGitDemo
+//
+//  Created by LGSC on 2017/9/13.
+//  Copyright © 2017年 LGSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LGGitDemo/LGGitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LGGitDemo
+//
+//  Created by LGSC on 2017/9/13.
+//  Copyright © 2017年 LGSC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
